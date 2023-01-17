347a0a1787e07d3b4291146425433d5fafe916fd
Humboorgir <hardcraftap@gmail.com>
2023-01-17 23:10:30 +0330

    installed tailwind, removed most css files

--- pages/home.js
+++ pages/home.js
@@ -1,53 +1,44 @@
-import Head from "next/head";
-import HomePage from "../components/Home/HomePage";
-import styles from "../styles/Home.module.css";
+import Head from "../components/head";
 import { useState } from "react";
-import { motion } from "framer-motion";
-import { FaExclamationTriangle } from "react-icons/fa";
+import Header from "../components/Home/header";
+import Hero from "../components/Home/hero";
+import Footer from "../components/footer";
+import RegisterModal from "../components/RegisterModal/index";
+import LoginModal from "../components/LoginForm/LoginModal";
+import Alert from "../components/Home/alert";
 export default function Home() {
-  // useEffect(() => {
-  //   const observer = new IntersectionObserver((entries) => {
-  //     entries.forEach((entry) => {
-  //       if (entry.isIntersecting) {
-  //         if (entry.target.classList.contains(styles.hiddenLeft)) {
-  //           entry.target.classList.add(styles.showLeft);
-  //         }
-  //         if (entry.target.classList.contains(styles.hiddenRight)) {
-  //           entry.target.classList.add(styles.showRight);
-  //         }
-  //         if (entry.target.classList.contains(styles.hiddenTop)) {
-  //           entry.target.classList.add(styles.showTop);
-  //         }
-  //         if (entry.target.classList.contains(styles.hiddenBottom)) {
-  //           entry.target.classList.add(styles.showBottom);
-  //         }
-  //       }
-  //     });
-  //   });
-  //   const hiddenLeft = document.querySelectorAll(`.${styles.hiddenLeft}`);
-  //   hiddenLeft.forEach((element) => observer.observe(element));
-  //   const hiddenRight = document.querySelectorAll(`.${styles.hiddenRight}`);
-  //   hiddenRight.forEach((element) => observer.observe(element));
-  //   const hiddenTop = document.querySelectorAll(`.${styles.hiddenTop}`);
-  //   hiddenTop.forEach((element) => observer.observe(element));
-  //   const hiddenBottom = document.querySelectorAll(`.${styles.hiddenBottom}`);
-  //   hiddenBottom.forEach((element) => observer.observe(element));
-  //   setTimeout(() => {
-  //     const gogolbtn = document.querySelector(`.${styles.gogolchatbtn}`);
-  //     gogolbtn.classList.remove(styles.gogolbtn);
-  //     gogolbtn.classList.remove(styles.hiddenLeft);
-  //     gogolbtn.classList.remove(styles.showLeft);
-  //   }, 1600);
-  // }, []);
+  const [regModalOpen, setRegModalOpen] = useState(false);
+  const [logModalOpen, setLogModalOpen] = useState(false);
+  const [alert, setAlert] = useState({
+    display: false,
+    ok: false,
+    content: "",
+  });
   return (
     <>
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Velocify ~ Home</title>
-      </Head>
-      <HomePage styles={styles} />
+      <Head page="Home" />
+      <>
+        <Alert alert={alert} />
+        <RegisterModal
+          setAlert={setAlert}
+          regModalOpen={regModalOpen}
+          setRegModalOpen={setRegModalOpen}
+        />
+        <Header
+          openRegModal={() => setRegModalOpen(true)}
+          openLogModal={() => setLogModalOpen(true)}
+        />
+        <Hero />
+        <Footer />
+        {logModalOpen && (
+          <LoginModal
+            setRegModal={setRegModalOpen}
+            setLogModal={setLogModalOpen}
+            setAlert={setAlert}
+            handleClose={() => setLogModalOpen(false)}
+          />
+        )}
+      </>
     </>
   );
 }
