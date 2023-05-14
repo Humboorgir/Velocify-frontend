cbe0b519520278274d833d2572be497beed4f49d
Humboorgir <hardcraftap@gmail.com>
2023-05-14 18:13:22 +0330

    removed login and register modals in home.js

--- components/ChatBox/Message.js
+++
@@ -1,9 +0,0 @@
-import { FaTimes } from "react-icons/fa";
-const Message = ({ styles, message }) => {
-  return (
-    <div className={styles.message}>
-      {message.text} <FaTimes style={{ display: "block", float: "right" }} />
-    </div>
-  );
-};
-export default Message;
--- pages/home.js
+++ pages/home.js
@@ -3,12 +3,8 @@ import { useState } from "react";
 import Header from "../components/Home/header";
 import Hero from "../components/Home/hero";
 import Footer from "../components/footer";
-import RegisterModal from "../components/RegisterModal/index";
-import LoginModal from "../components/LoginForm/LoginModal";
 import Alert from "../components/Home/alert";
 export default function Home() {
-  const [regModalOpen, setRegModalOpen] = useState(false);
-  const [logModalOpen, setLogModalOpen] = useState(false);
   const [alert, setAlert] = useState({
     display: false,
     ok: false,
@@ -19,25 +15,12 @@ export default function Home() {
       <Head page="Home" />
       <>
         <Alert alert={alert} />
-        <RegisterModal
-          setAlert={setAlert}
-          regModalOpen={regModalOpen}
-          setRegModalOpen={setRegModalOpen}
-        />
         <Header
           openRegModal={() => setRegModalOpen(true)}
           openLogModal={() => setLogModalOpen(true)}
         />
         <Hero />
         <Footer />
-        {logModalOpen && (
-          <LoginModal
-            setRegModal={setRegModalOpen}
-            setLogModal={setLogModalOpen}
-            setAlert={setAlert}
-            handleClose={() => setLogModalOpen(false)}
-          />
-        )}
       </>
     </>
   );
