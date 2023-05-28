d24a25655690cbdd0e89c5f6fb30bf1050d06d07
Humboorgir <hardcraftap@gmail.com>
2023-05-28 13:38:40 +0330

    remove username field from login

--- components/login/credentialslogin.js
+++ components/login/credentialslogin.js
@@ -1,4 +1,3 @@
-import Username from "@/components/login/username";
 import Email from "@/components/login/email";
 import Password from "@/components/login/password";
 import Submit from "@/components/login/submit";
@@ -6,10 +5,9 @@ import Submit from "@/components/login/submit";
 const CredentialsLogin = ({ isProcessing }) => {
   return (
     <div
-      className="relative flex w-[90%] flex-col flex-wrap items-center justify-center gap-x-8 
-  gap-y-4 border-b-[3px] border-b-yellowStrong pb-8 md:w-fit md:border-r-[3px] md:border-b-0 md:border-r-yellowStrong md:pr-8"
+      className="relative flex w-[90%] flex-col flex-wrap items-center justify-center gap-x-8 gap-y-4 
+  border-b-[3px] border-b-yellowStrong pb-8 md:mt-[5px] md:w-fit md:border-r-[3px] md:border-b-0 md:border-r-yellowStrong md:pr-8"
     >
-      <Username />
       <Email />
       <Password />
       <Submit isProcessing={isProcessing} />
