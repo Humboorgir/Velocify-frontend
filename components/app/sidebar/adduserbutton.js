e7b673297a9ecaccddcd22ed6efc2d2873d8b839
Humboorgir <hardcraftap@gmail.com>
2023-08-24 14:36:39 +0330

    Create sidebar/adduserbutton component

---
+++ components/app/sidebar/adduserbutton.js
@@ -0,0 +1,14 @@
+import { AiFillPlusCircle as Plus } from "react-icons/ai";
+
+const AddUserButton = () => {
+  return (
+    <div className="absolute bottom-8 right-6 z-10">
+      <div className="relative h-[60px] w-[60px]">
+        <Plus className="absolute z-20 h-[60px] w-[60px] cursor-pointer text-neutral-600" />
+        <div className="absolute top-0 left-0 right-0 bottom-0 m-1.5 flex rounded-full bg-white" />
+      </div>
+    </div>
+  );
+};
+
+export default AddUserButton;
--- components/app/sidebar/index.js
+++ components/app/sidebar/index.js
@@ -1,5 +1,6 @@
 import Users from "@/components/app/sidebar/users";
 import SidebarHeader from "@/components/app/sidebar/sidebarheader";
+import AddUserButton from "@/components/app/sidebar/adduserbutton";
 import { useRouter } from "next/router";
 
 const Sidebar = ({ users }) => {
@@ -7,10 +8,12 @@ const Sidebar = ({ users }) => {
   const isOnApp = router.pathname === "/app";
   return (
     <div
-      className={`top-0 left-0 h-screen w-[95vw] flex-shrink-0 flex-col bg-[#212121] py-4
+      className={`relative top-0 left-0 h-screen w-[95vw] flex-shrink-0 flex-col bg-[#212121] py-4
       md:max-w-[400px]
-      ${isOnApp ? "" : "hidden"} ${isOnApp ? "" : "md:"}flex `}>
+      ${isOnApp ? "" : "hidden"} ${isOnApp ? "" : "md:"}flex`}>
       <SidebarHeader />
+
+      <AddUserButton />
 
       <Users users={users} />
     </div>
