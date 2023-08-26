f0c618542252f2ac28d4e4706ae9d357bf5b9fce
Humboorgir <hardcraftap@gmail.com>
2023-08-26 12:24:13 +0330

    Make sidebar/adduserbutton a fixed element

--- components/app/sidebar/adduserbutton.js
+++ components/app/sidebar/adduserbutton.js
@@ -2,13 +2,15 @@ import { AiFillPlusCircle as Plus } from "react-icons/ai";
 
 const AddUserButton = () => {
   return (
-    <div className="absolute bottom-8 right-6 z-10">
-      <div className="relative h-[60px] w-[60px]">
-        <Plus
-          className="absolute z-20 h-[60px] w-[60px] cursor-pointer text-neutral-600
+    <div className="absolute bottom-[80px] right-[74px] z-10">
+      <div className="fixed">
+        <div className="relative h-[60px] w-[60px]">
+          <Plus
+            className="absolute z-20 h-[60px] w-[60px] cursor-pointer text-neutral-600
          hover:text-neutral-700 active:text-neutral-600"
-        />
-        <div className="absolute top-0 left-0 right-0 bottom-0 m-1.5 flex rounded-full bg-white" />
+          />
+          <div className="absolute top-0 left-0 right-0 bottom-0 m-1.5 flex rounded-full bg-white" />
+        </div>
       </div>
     </div>
   );
