ec7e4e87d8678181045ece50dcad4ded61bf0133
Humboorgir <hardcraftap@gmail.com>
2023-09-28 22:24:04 +0330

    Modify 'return' button on register and login pages

--- components/register/return.js
+++ components/register/return.js
@@ -3,12 +3,11 @@ import { MdArrowBackIosNew } from "react-icons/md";
 const Return = () => {
   return (
     <Link
-      href="/"
+      href="/login"
       className="absolute left-7 top-5 hidden flex-row items-center justify-center gap-1 pb-1  pr-1
-   text-textColorSemiWeak hover:border-b-2 hover:border-textColorSemiWeak md:left-12 md:top-10 md:flex"
-    >
+   text-textColorSemiWeak hover:border-b-2 hover:border-textColorSemiWeak md:left-12 md:top-10 md:flex">
       <MdArrowBackIosNew className="md:text-[20px]" />
-      <span className="text-[18px]">Go back</span>
+      <span className="text-[18px]">Log in</span>
     </Link>
   );
 };
